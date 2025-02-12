20ba4601f4dba272359099707309926da42f76e8
Christopher Sacré <christopher.sacre@keytradebank.com>
2025-02-12 15:15:41 +0100

    Implement complete CRUD of recipe.

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import get_recipe
+from .views import get_recipes, create_recipe, recipe
 
 urlpatterns = [
-  path('recipes', get_recipe, name = 'get_recipe')
+  path('recipes', get_recipes, name = 'get_recipe'),
+  path('recipes/create', create_recipe, name='create_recipe'),
+  path('recipes/<int:id>', recipe, name='recipe')
 ]
--- api/views.py
+++ api/views.py
@@ -7,5 +7,37 @@ from .serializer import RecipeSerializer
 
 # Create your views here.
 @api_view(['GET'])
-def get_recipe(request):
-  return Response(RecipeSerializer({ 'stars': 3, 'title': 'Hello World!' }).data)
+def get_recipes(request):
+  recipes = Recipe.objects.all()
+  serializer = RecipeSerializer(recipes, many=True)
+  return Response(serializer.data)
+
+@api_view(['POST'])
+def create_recipe(request):
+  serializer = RecipeSerializer(data=request.data)
+  if (serializer.is_valid()):
+    serializer.save()
+    return Response(serializer.data, status = status.HTTP_201_CREATED)
+  return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def recipe(request, id):
+  try:
+    recipe = Recipe.objects.get(pk=id)
+  except Recipe.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+  
+  if request.method == 'GET':
+    serializer = RecipeSerializer(recipe)
+    return Response(serializer.data)
+  
+  if request.method == 'PUT':
+    serializer = RecipeSerializer(recipe, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
+  if request.method == 'DELETE':
+    recipe.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
