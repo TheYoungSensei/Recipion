05f87c469fbbf18c9c128610db6ab5655a9398b9
Christopher Sacré <christopher.sacre@keytradebank.com>
2025-03-17 15:21:33 +0100

    [FRONTEND] Start to write down the recipe detail screen.

--- frontend/app/components/recipes/Recipe.tsx
+++ frontend/app/components/recipes/Recipe.tsx
@@ -1,5 +1,4 @@
-"use client"
-
+import { Text, Title } from '@mantine/core'
 import { useRecipe } from "../../hooks/useRecipes"
 
 type TProps = {
@@ -10,7 +9,10 @@ const Recipe = ({ id }: TProps) => {
   const { data } = useRecipe(id)
 
   return (
-    <p>{JSON.stringify(data)}</p>
+    <div style={{ marginLeft: 16, marginRight: 16 }}>
+      <Title order={1}>{data?.title}</Title>
+      <Text style={{ marginTop: 8 }} c='dimmed'>{data?.subTitle}</Text>
+    </div>
   )
 }
 
