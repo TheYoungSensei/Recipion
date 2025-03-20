26ab2eb377232f032a2ea74449ed545c822b3e37
Christopher Sacré <sacre.christopher@hotmail.com>
2025-03-20 17:13:15 +0100

    Implement DELETE endpoint.

---
+++ backend/src/recipes/recipes.dto.ts
@@ -0,0 +1,7 @@
+export class CreateRecipeDto {
+  title: string;
+  stars: number;
+  subTitle: string;
+  coverImage: string;
+  text: string;
+}
--- backend/src/recipes/recipes.service.ts
+++ backend/src/recipes/recipes.service.ts
@@ -1,18 +1,26 @@
-import { Injectable } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Recipe } from "./recipes.schema";
-import { Model } from "mongoose";
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Recipe } from './recipes.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class RecipesService {
   constructor(@InjectModel(Recipe.name) private recipeModel: Model<Recipe>) {}
 
   async create(recipe): Promise<Recipe> {
-    const created = new this.recipeModel(recipe)
-    return created.save()
+    const created = new this.recipeModel(recipe);
+    return created.save();
   }
 
-  async findAll(): Promise<Recipe[]>{
-    return this.recipeModel.find().exec()
+  async findAll(): Promise<Recipe[]> {
+    return this.recipeModel.find().exec();
+  }
+
+  async findById(id: string): Promise<Recipe | null> {
+    return this.recipeModel.findById(id);
+  }
+
+  async delete(id: string): Promise<Recipe | null> {
+    return this.recipeModel.findOneAndDelete({ _id: id });
   }
 }
